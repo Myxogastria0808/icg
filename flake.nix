2d27fda23df2bd5e12a3c274617a0915a4f0ab08
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-04-17 22:27:26 +0900

    feat: build successful！

--- flake.nix
+++ flake.nix
@@ -1,81 +1,33 @@
 {
+  description = "nodejs flake sample";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        libcIncludeDir =
-          if pkgs.stdenv.isDarwin
-          then "${pkgs.darwin.Libsystem}/include"
-          else "${pkgs.glibc.dev}/include";
-        toSingleLine = str: builtins.replaceStrings ["\n"] [" "] str;
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [
-              nil
-              alejandra
-              llvmPackages.clang-unwrapped
-              python311Packages.compiledb
-              gnumake
-              cmake
-              typst
-              typstfmt
-              typst-lsp
-              typst-live
-              typst-preview
-              xorg.libX11
-              xorg.libXext
-              xorg.libXrender
-              xorg.libXt
-              libpng
-              libjpeg
-              fltk
-              libGL
-              libGLU
-              pkg-config
-              pango cairo
-            ];
 
-          NIX_CFLAGS_COMPILE = with pkgs;
-            toSingleLine ''
-              -I${llvmPackages.clang-unwrapped.lib}/lib/clang/16/include
-              -I${llvmPackages.libcxx.dev}/include/c++/v1
-              -I${llvmPackages.libcxxClang}/resource-root/include
-              -I${llvmPackages.libcxxStdenv}
-              -I${libpng.dev}/include
-              -I${fltk}/include
-              -I${libcIncludeDir}
-            '';
-
-          NIX_LDFLAGS = with pkgs;
-            toSingleLine ''
-              -L${libpng}/lib
-              -lpng
-              -L${fltk}/lib
-              -lfltk
-              -lfltk_images
-              -L${xorg.libX11}/lib
-              -lX11
-              -lXext
-              -ljpeg
-              -lfltk_gl
-              -lfltk_forms
-              -lm
-            '';
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            fltk
+            gcc
+            cmake
+            # xorg.libX11
+            # xorg.libXft
+            # xorg.libXext
+            # zlib
+            # libpng
+            doxygen
+            pkg-config
+            libGL
+            libGLU
+          ];
         };
       }
     );
